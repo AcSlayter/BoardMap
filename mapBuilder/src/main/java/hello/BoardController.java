8d58a62fce683958853e9647860565ce2aa62a66
Aaron <aaronbcain@gmial.com>
2018-10-23 21:33:38 -0700

    Adding new endpoint

--- mapBuilder/src/main/java/hello/BoardController.java
+++ mapBuilder/src/main/java/hello/BoardController.java
@@ -3,6 +3,7 @@ package hello;
 import hello.something.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -15,6 +16,12 @@ public class BoardController {
     @GetMapping("/test")
     @ResponseBody
     public Board gretting() {
+        return new Board();
+//        return "test";
+    }
+    @GetMapping("/test/{x}/{y}")
+    @ResponseBody
+    public Board grettings(@PathVariable int x,@PathVariable int y ) {
         return new Board();
 //        return "test";
     }
